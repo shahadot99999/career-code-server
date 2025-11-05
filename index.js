6265c699a25462deda8b901a6cf7d4a7a04b3b3d
shahadot99999 <shahadot99999@gmail.com>
2025-11-05 21:19:01 +0600

    Add Jobs Data and Load data server

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection = client.db('careerCode').collection('jobs');
+    //jobs api
+    app.get('/jobs', async (req, res)=>{
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
