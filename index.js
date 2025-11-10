5b6c7057509b3f9c4b6f7e08393dc1e7e8dd9de4
shahadot99999 <shahadot99999@gmail.com>
2025-11-10 17:16:54 +0600

    Create Job detail Api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 //middleware
@@ -32,6 +32,14 @@ async function run() {
     app.get('/jobs', async (req, res)=>{
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //jobs particular id
+    app.get('/jobs/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     })
 
