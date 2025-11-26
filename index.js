56a024b2e9fe6366f9249715326031aa0e331096
shahadot99999 <shahadot99999@gmail.com>
2025-11-26 18:38:31 +0600

    Update Job Application Status apply in patch method in server side

--- index.js
+++ index.js
@@ -110,6 +110,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/applications/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          status: req.body.status
+        }
+      }
+      const result = await applicationsCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
